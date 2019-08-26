ee4573d6ea996bec39c04d6a28f20b4783bf35b4
talgropper <tal.gropper@weissbeerger.com>
2019-08-26 15:36:27 +0300

    -new pool settings

--- Spring Projects/homeServer/src/main/java/io/homecloud/homeserver/homeServerApp.java
+++ Spring Projects/homeServer/src/main/java/io/homecloud/homeserver/homeServerApp.java
@@ -1,30 +1,74 @@
 package io.homecloud.homeserver;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import Filters.CORSFilter;
 
 @SpringBootApplication
 public class homeServerApp {
 
 	public static final String ANSI_RED = "\u001B[31m";
 	public static final String ANSI_RESET = "\u001B[0m";
-	
-	public static void main(String[] args) {
-		SpringApplication.run(homeServerApp.class, args);
-		
-		//This is for making localHost public
-		//Access at danie.localhost.run
+
+	public static void run(String[] args, String userName, String serverId, String awsDomain) {
+
+		SpringApplication application = new SpringApplication(homeServerApp.class);
+
+		//adding properties for 
+		Properties properties = new Properties();
+		properties.put("spring.http.multipart.max-file-size", "700MB");
+		properties.put("spring.http.multipart.max-request-size", "700MB");
+		application.setDefaultProperties(properties);
+
+		application.run(args);
+
+		//run ssh to do remote port forwarding
 		try {
 			Process p = Runtime.getRuntime().exec("ssh -R 80:localhost:8080 ssh.localhost.run");
-			String userName = System.getProperty("user.name");
-			System.out.println(ANSI_RED + "\n\n Access to server is at: http://"+userName+".localhost.run\n\n"+ ANSI_RESET);
-			
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String s = null;
+			System.out.println("\n\n");
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(ANSI_RED + s + ANSI_RESET);
+				String[] splitcmdOutPut = s.split(" ");
+				String domain = splitcmdOutPut[4];
+				//TODO: send awsDomain userName, serverId , domain
+				//System.out.println("userName: " + userName + ", serverId: " + serverId + ", AWSdomain:" + awsDomain + ", homeServer domain: " + domain);
+			}
+
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			while ((s = stdError.readLine()) != null) {
+				System.out.println(s);
+			}
+
 		} catch (IOException e) {
 			System.out.println("Could not run \"ssh -R 80:localhost:8080 ssh.localhost.run\" command");
 			e.printStackTrace();
 		}
+
+
 	}
+
+	//This is for creating the CORS filter
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration() {
+		FilterRegistrationBean registrationBean =
+				new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
+	}
+
+
 
 }
---
+++ Spring Projects/homeServer/src/main/java/io/homecloud/homeserver/localFile/homeServerStarter.java
@@ -0,0 +1,153 @@
+package io.homecloud.homeserver.localFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import io.homecloud.homeserver.homeServerApp;
+
+public class homeServerStarter {
+
+	public static void run(String[] args, String userName) {
+
+		String homeServerDir = createMainAppFolders();
+
+		creatUserFolder(homeServerDir, userName);
+
+		String serverId = userName + "-homeServer";
+		createXML(homeServerDir, serverId);
+
+		HashMap<String, HashMap<String, String>> config = readXML(homeServerDir + File.separator + "config.xml");
+		
+		serverId = config.get("homeServer").get("server-id");
+		String awsDomain = config.get("AWS").get("domain");
+
+		homeServerApp.run(args, userName,serverId, awsDomain);
+
+	}
+
+	private static HashMap<String, HashMap<String, String>> readXML(String xmlFile) {
+		HashMap<String, HashMap<String, String>> rtn = new HashMap<String, HashMap<String, String>>();
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(xmlFile);
+			Element root = doc.getDocumentElement();
+			NodeList nlist = root.getChildNodes();
+			HashMap<String, String> tempList;
+			for (int temp = 0; temp < nlist.getLength(); temp++) {
+				tempList = new HashMap<String, String>();
+				Node nNode = nlist.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					String nodeName = nNode.getNodeName();
+
+					NodeList n2list = nNode.getChildNodes();
+					for (int temp2 = 0; temp2 < n2list.getLength(); temp2++) {
+						Node n2Node = n2list.item(temp2);
+						if (n2Node.getNodeType() == Node.ELEMENT_NODE)
+						tempList.put(n2Node.getNodeName(), n2Node.getTextContent());
+					}
+					rtn.put(nodeName, tempList);
+				}
+			}
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return rtn;
+	}
+
+	private static void createXML(String homeServerDir, String serverId) {
+		//exit if config file exists
+		if(new File(homeServerDir + File.separator + "config.xml").exists())
+			return;
+		DocumentBuilderFactory icFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder icBuilder;
+		try {
+			icBuilder = icFactory.newDocumentBuilder();
+			Document doc = icBuilder.newDocument();
+			Element root = doc.createElement("config");
+
+			//homeServer Node
+			Element serverInfoMainNode = doc.createElement("homeServer");
+			Element serverIdElem = doc.createElement("server-id");
+			serverIdElem.appendChild(doc.createTextNode(serverId));
+			serverInfoMainNode.appendChild(serverIdElem);
+			root.appendChild(serverInfoMainNode);
+
+			//AWS Node
+			Element serverAWSMainNode = doc.createElement("AWS");
+			Element awsDomain = doc.createElement("domain");
+			awsDomain.appendChild(doc.createTextNode("www.aws.com"));
+			serverAWSMainNode.appendChild(awsDomain);
+			root.appendChild(serverAWSMainNode);
+
+			doc.appendChild(root);
+
+			try {
+				Transformer tr = TransformerFactory.newInstance().newTransformer();
+				tr.setOutputProperty(OutputKeys.INDENT, "yes");
+				tr.setOutputProperty(OutputKeys.METHOD, "xml");
+				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+				// send DOM to file
+				tr.transform(new DOMSource(doc), 
+						new StreamResult(new FileOutputStream(homeServerDir + File.separator + "config.xml")));
+			}  catch (TransformerException te) {
+				System.out.println("tr");
+				System.out.println(te.getMessage());
+			} catch (IOException ioe) {
+				System.out.println("IO");
+				System.out.println(ioe.getMessage());
+			}
+
+		} catch (ParserConfigurationException e) {
+			System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + e);
+		}
+
+	}
+
+	//a method to create user dir if not exists
+	private static void creatUserFolder(String homeServerDir, String userName) {
+		File userDir = new File(homeServerDir + File.separator + "root" + File.separator + userName);
+		if(!userDir.exists())
+			userDir.mkdir();
+	}
+
+	//a method to create main app folders
+	//return main homeServer dir
+	public static String createMainAppFolders() {
+		//get name of main root (C:/)
+		File homeServerDir = new File(System.getProperty("user.home"));
+		homeServerDir = homeServerDir.getParentFile();
+		homeServerDir = homeServerDir.getParentFile();
+		homeServerDir = new File(homeServerDir.getPath() + File.separator + "homeServer");
+		if(!homeServerDir.exists())
+			homeServerDir.mkdir();
+		String rtn = homeServerDir.getPath();
+		File homeServerRoot = new File(homeServerDir.getPath() + File.separator + "root");
+		if(!homeServerRoot.exists())
+			homeServerRoot.mkdir();
+		return rtn;
+	}
+
+}
